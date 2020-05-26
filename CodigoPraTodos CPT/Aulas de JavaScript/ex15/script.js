05f3d902a94c3da888b021b6628f3b7f9d064325
Carlos Junior <carlostrader09@gmail.com>
2020-05-26 15:59:36 +0100

    Aulas de JavaScript

--- CodigoPraTodos CPT/Aulas de JavaScript/ex15/script.js
+++ CodigoPraTodos CPT/Aulas de JavaScript/ex15/script.js
@@ -1,1 +1,18 @@
-alert("Olá, Mundo!")
+function carregar() {
+    var msg = window.document.getElementById("msg");
+    var img = window.document.getElementById("imagem");
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // Bom Dia!
+        img.src = "fotomanha.jpg"
+    } else if (hora >= 12 && hora <= 18) {
+        // Boa Tarde!
+        img.src = "fototarde.jpg"
+    } else {
+        // Boa Noite!
+        img.src = "fotonoite.jpg"
+    }
+}
+
