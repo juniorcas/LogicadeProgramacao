069bba6aec9c7db6127830e4c02711540cd3c56c
Carlos Junior <carlostrader09@gmail.com>
2020-06-11 11:09:36 +0100

    Meu Projeto 2

---
+++ CodigoPraTodos CPT/Horario das Bolsas/script.js
@@ -0,0 +1,9 @@
+function carregar() {
+    var msg = document.getElementById("msg");
+    var img = document.getElementById("imagem");
+    var data = new Date();
+    var hora = data.getHours();
+    var min = data.getMinutes();
+    var sec = data.getSeconds();
+}
+
